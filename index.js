2202d92611d5bab9ddaf9737f5cb33041f36b733
ivanrodriguez <ivanoviicrm2@gmail.com>
2020-07-25 13:27:20 +0200

    js

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
 const main = () => {
-    console.log('loaded js');
+    const addTaskIcon = document.querySelector("#addTaskIcon");
+    const addTaskInput = document.querySelector("#addTaskInput");
+    addTaskIcon.addEventListener('click', addTask);
+    addTaskInput.addEventListener('keydown', ($event) => {
+        if (event.key === "Enter") {
+            addTask();
+        }
+    });
+}
+
+const addTask = () => {
+    const addTaskInput = document.querySelector("#addTaskInput");
+    const inputValue = addTaskInput.value;
+    const taskList = document.querySelector("#taskList");
+
+    // TODO - añadir a la lista el valor del input aqui
+    console.log(inputValue);
+
+    addTaskInput.value = null;
 }
 
 window.onload = main;
