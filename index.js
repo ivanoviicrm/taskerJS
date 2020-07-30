2a5d66c9db2dc3f7be469fdcdb546d202ca0c71a
ivanrodriguez <ivanoviicrm2@gmail.com>
2020-07-30 21:21:59 +0200

    working on js

--- index.js
+++ index.js
@@ -9,14 +9,34 @@ const main = () => {
     });
 }
 
+const removeTask = (div) => {
+    console.log(div);
+}
+
 const addTask = () => {
     const addTaskInput = document.querySelector("#addTaskInput");
     const inputValue = addTaskInput.value;
     const taskList = document.querySelector("#taskList");
 
-    // TODO - añadir a la lista el valor del input aqui
-    console.log(inputValue);
+    // Nothing happens if value is empty
+    if (!inputValue) {
+        return;
+    }
 
+    // Task is added to the list
+    const div = document.createElement('div');
+    div.classList.add("task");
+    const content = `
+      <input type="checkbox" class="task__check"/>
+      <p class="task__text">${inputValue}</p>
+      <div class="task__actions" onclick="removeTask(div)">
+        <i class="fas fa-trash-alt task__delete"></i>
+      </div>
+    `
+    div.innerHTML = content;
+    const lastContent = taskList.appendChild(div);
+
+    // Input reset
     addTaskInput.value = null;
 }
 
